83fb59649849b45e89daacd1b7921da48a7a0e7c
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-22 16:40:24 +0500

    update 59_square_calculating.py

---
+++ 59_square_calculating.py
@@ -0,0 +1,6 @@
+def square_calculating():
+  """ a function that accpets user's numbers to calculate its square"""
+  num = float(input("Which number's square do you need? "))
+  result = num ** 2
+  print(f"{num} squared is {result}")
+square_calculating()
