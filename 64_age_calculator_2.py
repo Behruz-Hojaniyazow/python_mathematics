d1a6f439c8ca609dbdf380ca0c28c07fabfd6a8f
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-23 20:40:14 +0500

    update 64_age_calculator_2.py

---
+++ 64_age_calculator_2.py
@@ -0,0 +1,4 @@
+def calculate_age(born_year, name):
+  """ a function that calculates the user's age"""
+  print(f"Hello {name.title()}, you are {2026 - born_year} years old")
+calculate_age(2008, 'behruz')
