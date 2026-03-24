507f8608bbe4010647244003e2cc64c88d0a7331
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-25 00:53:17 +0500

    update 72_find_half.py

---
+++ 72_find_half.py
@@ -0,0 +1,13 @@
+def find_half(a):
+  """a function thag finds the half of the given number"""
+  num = a / 2
+  return num
+while True:
+  try:
+    number = float(input("Enter a number to find half of a number: "))
+    break
+  except ValueError:
+    print("Please enter only numbers")
+    
+result = find_half(number)
+print(f"The half of {number} is {result}")
