1972b7bab1291f1cdbce3a7dd42439520c42f0fe
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-04-03 19:48:04 +0500

    update 95_multiply_all_numbers.py WIP

---
+++ 95_multiply_all_numbers.py
@@ -0,0 +1,30 @@
+def get_number(prompt):
+  """a function that only accepts numbers from the user"""
+  while True:
+    try:
+      return float(input(prompt))
+    except ValueError:
+      print('Please enter only numbers, Try again')
+def multiply(*args):
+  """a function that multiplies all numberss"""
+  if not args:
+    return 1
+  num = 1
+  for number in args:
+    num *= number
+  return num
+while True:
+  numbers = []
+  choice = get_number("Enter a number (or 'done' to finish): ")
+  if choice.lower() == "done":
+    break
+  try:
+    numbers.append(choice)
+  except ValueError:
+    print("Invalid input, Try Again")
+  if numbers:
+    result = multiply(*numbers)
+  number_str = " × ".join(map(str, numbers))
+  print(f"{number_str} = {result}")
+  if input("Do you wanna continue (yes/no)?").lower() == "no":
+    break
