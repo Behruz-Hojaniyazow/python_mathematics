29cdc2603a6371e9f2d745415d781a38ac85eaf3
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-04-03 16:29:32 +0500

    update 94_longest_word.py

--- 94_longest_word.py
+++ 94_longest_word.py
@@ -1,16 +1,18 @@
 def analyze_text(texts):
   """a function that finds the longest word in the list"""
-  longest_word = ""
   longest_length = 0
+  longest_words = []
   for text in texts:
     count = 0
     for letter in text:
       count += 1
     print(f"\n{text} has {count} letters")
     if count > longest_length:
-      longest_word = text
       longest_length = count
-  return longest_word, longest_length
+      longest_words = [text]
+    elif count == longest_length and count != 0:
+      longest_words.append(text)
+  return longest_words, longest_length
 def main():
   words = []
   while True:
@@ -22,11 +24,20 @@ def main():
       words.append(word)
   if words:
     word, length = analyze_text(words)
-    print("=" * 30)
-    print(f"The longest word is {word}")
-    print("-" * 30)
-    print(f"{word} has {length} letters")
-    print("=" * 30)
+    if len(word) > 1:
+      print(f"\nThe longest words have {length} letters")
+      print("These are the longest texts")
+      print("=" * 30)
+      for w in word:
+        print(f"{w} | {length} letter")
+        print("-" * 30)
+    else:
+      name = word[0]
+      print(f"\nThe longest word has {length} letters")
+      print("This is the only longest text")
+      print("="  * 30)
+      print(f"{name} | {length} letter")
+      print("-"  * 30)
   else:
     print("No information entered")
 if __name__ == "__main__":
