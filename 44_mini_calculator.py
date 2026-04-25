708a458056e0bdc2d36a1ef1e29e5d1d09bc4147
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-04-25 17:08:20 +0500

    feat: complete calculator logic, add history feature and fix zero division error

--- 44_mini_calculator.py
+++ 44_mini_calculator.py
@@ -12,6 +12,7 @@ def get_numbers(prompt):
     except ValueError:
       print("Please, Enter only numbers or type 'stop' to finish")
   return numbers
+  
 def main():
   memory = {"history" : []}
   while True:
@@ -19,11 +20,12 @@ def main():
       print("\n---Menu---")
       print("1. press 1 to add numbers")
       print("2. press 2 to multiply numbers")
-      print("3. press 3 to stop the project")
-      choice = int(input("Choose an action (1/2/3)"))
-      user_input = input("Do you want to continue? (yes/no)")
-      if user_input.lower() == 'no':
-        break
+      print("3. press 3 to substract numbers")
+      print("4. press 4 to division numbers")
+      print("5. press 5 to display the history")
+      print("6. press 6 to stop the project")
+      choice = int(input("Choose an action (1/2/3/4/5/6)"))
+      
       if choice == 1:
         a = get_numbers("enter a number: ")
         if len(a) > 1:
@@ -34,21 +36,76 @@ def main():
           action_string = f"{clean_line} = {display_sum}"
           memory['history'].append(action_string)
           print(action_string)
-          print(f"Result: {action_string}. action was added to memory")
+          print(f"Sum: {action_string} | action was added to memory")
         else:
           print("You must enter at least 2 numbers to start the process")
+          
       elif choice == 2:
-        a = float(input("enter the first number: "))
-        result = a * b
-        action_string = f"{a} × {b} = {result}"
-        memory['history'].append(action_string)
-        print(f"Result: {result}. action was added to memory")
+        a = get_numbers("enter a number: ")
+        if len(a) > 1:
+          formatted_numbers = [int(num) if num.is_integer() else num for num in a]
+          result = 1
+          for x in formatted_numbers:
+            result *= x
+          product = int(result) if result.is_integer() else result
+          clean_line = ' × '.join(map(str, formatted_numbers))
+          action_string = f"{clean_line} = {product}"
+          memory['history'].append(action_string)
+          print(f"Product: {action_string} | action was added to memory")
+        else:
+          print("You must enter at least 2 numbers to start the process")
+          
       elif choice == 3:
-        print("The project was stopped")
-        print(memory)
+        a = get_numbers("enter a number: ")
+        if len(a) > 1:
+          formatted_numbers = [int(num) if num.is_integer() else num for num in a]
+          difference = formatted_numbers[0]
+          for x in formatted_numbers[1:]:
+            difference -= x
+          result = int(difference) if difference.is_integer() else difference
+          clean_line = ' - '.join(map(str, formatted_numbers))
+          action_string = f"{clean_line} = {result}"
+          memory['history'].append(action_string)
+          print(f"Division: {action_string} | action was added to memory")
+        else:
+          print("You must enter at least 2 numbers to start the process")
+          
+      elif choice == 4:
+        a = get_numbers("enter a number: ")
+        if len(a) > 1:
+          formatted_numbers = [int(num) if num.is_integer() else num for num in a]
+          result = formatted_numbers[0]
+          is_zero_division = False
+          for x in formatted_numbers[1:]:
+            if x == 0:
+              print("\nError: Can not divide by zero! Operation aborted")
+              is_zero_division = True
+              break
+            result /= x
+          if not is_zero_division:
+            quotient = int(result) if result.is_integer() else result
+            clean_line = ' ÷ '.join(map(str, formatted_numbers))
+            action_string = f"{clean_line} = {quotient}"
+            memory['history'].append(action_string)
+            print(f"Quotient: {action_string} | action was added to memory")
+        else:
+          print("You must enter at least 2 numbers to start the process")
+          
+      elif choice == 5:
+        if memory['history']:
+          print("\n---History of Actions---")
+          for index, action in enumerate(memory['history'], 1):
+            print(f"{index}. {action}")
+        else:
+          print("\nHistory is empty. No actions have been performed yet.\n")
+          
+      elif choice == 6:
+        print("\nThe Project has been stopped, Bye!")
         break
+      
       else:
-        print("The wrong choice, please choose only 1, 2, or 3")
+        print("The wrong choice, Please select only actions 1 to 6")
+        
     except ValueError:
       print("please write only numbers")
 if __name__ == "__main__":
