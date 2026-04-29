06f6bb873683704e6691dea4e49e135ff5834fdc
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-04-29 13:06:45 +0500

    feat: add core arithmetic module with basic operations

---
+++ advanced_100_1000/arithmetic.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+  """function that adds the given numbers"""
+  return a + b
+  
+def subtraction(a, b):
+  """function subtracts the given numbers"""
+  return a - b
+
+def multiplication(a, b):
+  """funtion that multiplies the given numbers"""
+  return a * b
