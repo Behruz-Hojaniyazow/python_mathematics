aec096172f61648659a0b4f01bced0c9d9ffb13f
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-04-30 12:34:36 +0500

    ​feat: integrate geometry and number game modules into a unified super_app entry point

---
+++ advanced_100_1000/super_app.py
@@ -0,0 +1,40 @@
+import geometry_2 as geo
+import lucky_number as ln
+import sys
+
+def exit_app():
+  print("\n👋🏼Thank you for using the Kryos project! GoodBye!")
+  sys.exit()
+
+def start_menu():
+  """main menu and feature management"""
+  # We put the choices in the dictionary (Professional method) 
+  menu_actions = {
+    '1' : geo.main,
+    '2' : ln.find_ai_num,
+    '3' : exit_app
+  }
+  
+  while True:
+    print("\n" + "=" * 40)
+    print("--- 🚀 Kryos Multifunctional Sowftware ---")
+    print("=" * 40)
+    print("1. Use geometry tools")
+    print("2. Start lucky number game")
+    print("3. Stop the project")
+    print("-" * 40)
+    
+    choice = input("Choose an action: ")
+    
+    if choice in menu_actions:
+      # Lug'atdan funksiyani olib, uni ishga tushiramiz
+      menu_actions[choice]()
+    else:
+      print("\n❌️Error: Wrong choice! Please enter 1, 2 or 3")
+      
+if __name__ == "__main__":
+  try:
+    start_menu()
+  except KeyboardInterrupt:
+    #To prevent the program from giving an ugly error when pressing Ctrl+C
+    exit_app()
